1b362009d255e7e122b3e25aa8fced8cf47b193b
pcq129 <pcq129_tatva@outlook.com>
2025-07-22 09:35:52 +0530

    auto

--- app/Console/Commands/generateService.php
+++ app/Console/Commands/generateService.php
@@ -5,6 +5,8 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Filesystem\Filesystem;
+
 
 class generateService extends Command implements PromptsForMissingInput
 {
@@ -13,7 +15,8 @@ class generateService extends Command implements PromptsForMissingInput
      *
      * @var string
      */
-    protected $signature = 'make:service {serviceName}';
+    protected $signature = 'make:service {Service Name}';
+
 
     /**
      * The console command description.
@@ -27,22 +30,19 @@ class generateService extends Command implements PromptsForMissingInput
      */
     public function handle()
     {
-        $serviceName = $this->argument('serviceName');
-        if($serviceName){
+        $serviceName = $this->argument('Service Name');
+        if ($serviceName) {
             $serviceName = ucfirst($serviceName);
-            $filePath = '/Services/' . $serviceName . '.php';
+            $filePath = app_path('Services\\' . $serviceName . '.php');
             // dd($serviceName, $filePath);
 
-            if (file_exists($filePath)) {
-                $this->error("Service {$serviceName} already exists.");
-                return;
-            }
+            (new Filesystem)->ensureDirectoryExists(dirname($filePath));
 
             $stub = file_get_contents(app_path('Console/Commands/stubs/service.stub'));
             $stub = str_replace('{{ serviceName }}', $serviceName, $stub);
-            $stub = str_replace('{{namespace}}', 'App\Services', $stub);
+            $stub = str_replace('{{ namespace }}', 'App\Services', $stub);
 
-            Storage::disk('local')->put($filePath, $stub);
+            file_put_contents($filePath, $stub);
             $this->info("Service {$serviceName} created successfully. Path: {$filePath}");
         } else {
             $this->error('Service name is required.');
@@ -52,7 +52,7 @@ class generateService extends Command implements PromptsForMissingInput
     protected function promptForMissingArgumentsUsing(): array
     {
         return [
-            'serviceName' => "What'd you like to name the service?",
+            'Service Name' => "What'd you like to name the service?",
         ];
     }
 }
---
+++ app/Services/Adsfadsfadsfasdfa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\DTO\ServiceResponse;
+
+class Adsfadsfadsfasdfa
+{
+    /**
+     * Define service methods here.
+     */
+}
